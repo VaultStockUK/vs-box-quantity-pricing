7b2c5079ab7be23fbd0c22131de7cec2cec9ebdb
VaultStockUK <info@vaultstock.co.uk>
2026-08-14 01:31:02 +0100

    Wire display components

--- includes/class-vs-bqp-display.php
+++ includes/class-vs-bqp-display.php
@@ -4,7 +4,17 @@ defined( 'ABSPATH' ) || exit;
 
 class VS_BQP_Display {
     private $product_data;
+
     public function __construct( VS_BQP_Product_Data $product_data ) {
         $this->product_data = $product_data;
+    }
+
+    public function init() {
+        require_once VS_BQP_PATH . 'includes/class-vs-bqp-display-price.php';
+        require_once VS_BQP_PATH . 'includes/class-vs-bqp-display-cart.php';
+        require_once VS_BQP_PATH . 'includes/class-vs-bqp-display-variation.php';
+        ( new VS_BQP_Display_Price( $this->product_data ) )->init();
+        ( new VS_BQP_Display_Cart( $this->product_data ) )->init();
+        ( new VS_BQP_Display_Variation( $this->product_data ) )->init();
     }
 }
