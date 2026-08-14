898b58c001f11bf0f2ffded3ad6d8920f21edfe3
VaultStockUK <info@vaultstock.co.uk>
2026-08-14 01:29:22 +0100

    Implement bulk action wrapper

--- includes/bulk.php
+++ includes/bulk.php
@@ -1,2 +1,10 @@
 <?php
-// VS module.
+
+defined( 'ABSPATH' ) || exit;
+
+trait VS_BQP_Bulk_Action {
+    use VS_BQP_Bulk_Menu;
+    use VS_BQP_Admin_UI;
+    use VS_BQP_Admin_Ajax;
+    use VS_BQP_Admin_Bulk_Update;
+}
