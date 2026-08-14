41a598c3910099d706627a3f1356c3a9ae59c818
VaultStockUK <info@vaultstock.co.uk>
2026-08-14 01:28:16 +0100

    Add secured bulk update endpoint

--- includes/trait-vs-bqp-admin-ajax.php
+++ includes/trait-vs-bqp-admin-ajax.php
@@ -1,2 +1,18 @@
 <?php
-// VS BQP admin Ajax.
+
+defined( 'ABSPATH' ) || exit;
+
+trait VS_BQP_Admin_Ajax {
+    public function ajax_bulk_set_units_per_box() {
+        check_ajax_referer( 'vs_bqp_bulk_units', 'nonce' );
+        $product_id = isset( $_POST['product_id'] ) ? absint( $_POST['product_id'] ) : 0;
+        if ( ! $product_id || ! current_user_can( 'edit_post', $product_id ) ) {
+            wp_send_json_error( array( 'message' => __( 'You do not have permission to edit this product.', 'vs-box-quantity-pricing' ) ), 403 );
+        }
+        $product = wc_get_product( $product_id );
+        if ( ! $product instanceof WC_Product_Variable ) {
+            wp_send_json_error( array( 'message' => __( 'This bulk action is only available for variable products.', 'vs-box-quantity-pricing' ) ), 400 );
+        }
+        $this->apply_bulk_units_value( $product );
+    }
+}
