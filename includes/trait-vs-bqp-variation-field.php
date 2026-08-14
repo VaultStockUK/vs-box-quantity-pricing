bfc55932d1b9da57e8a421e4d91aff7592ad4004
VaultStockUK <info@vaultstock.co.uk>
2026-08-14 01:26:22 +0100

    Implement variation units field display

--- includes/trait-vs-bqp-variation-field.php
+++ includes/trait-vs-bqp-variation-field.php
@@ -1,2 +1,19 @@
 <?php
-// VS BQP variation field.
+
+defined( 'ABSPATH' ) || exit;
+
+trait VS_BQP_Variation_Field {
+    public function render_variation_field( $loop, $variation_data, $variation ) {
+        woocommerce_wp_text_input( array(
+            'id' => self::META_KEY . '[' . $loop . ']',
+            'name' => self::META_KEY . '[' . $loop . ']',
+            'value' => get_post_meta( $variation->ID, self::META_KEY, true ),
+            'label' => __( 'Units Per Box', 'vs-box-quantity-pricing' ),
+            'description' => __( 'Leave blank to inherit the parent product value. Enter 1 to disable box pricing for this variation.', 'vs-box-quantity-pricing' ),
+            'desc_tip' => true,
+            'type' => 'number',
+            'wrapper_class' => 'form-row form-row-full',
+            'custom_attributes' => array( 'min' => '1', 'step' => '1' ),
+        ) );
+    }
+}
