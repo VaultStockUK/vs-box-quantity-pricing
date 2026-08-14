25b5b711df6517a16c9157f9f40b3dd6ce061c87
VaultStockUK <info@vaultstock.co.uk>
2026-08-14 01:29:38 +0100

    Add internal module loader

---
+++ includes/modules.php
@@ -0,0 +1,15 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-product-field.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-variation-field.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-product-values.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-bulk-menu.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-admin-ui.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-admin-ajax.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-admin-bulk-update.php';
+require_once VS_BQP_PATH . 'includes/bulk.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-pricing-cart-data.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-pricing-calculation.php';
+require_once VS_BQP_PATH . 'includes/trait-vs-bqp-pricing-order-meta.php';
