326d865ce92e3f64671481bb577eb9d250a402f5
VaultStockUK <info@vaultstock.co.uk>
2026-08-14 01:30:50 +0100

    Add display class foundation

--- includes/class-vs-bqp-display.php
+++ includes/class-vs-bqp-display.php
@@ -1,2 +1,10 @@
 <?php
-// VS module.
+
+defined( 'ABSPATH' ) || exit;
+
+class VS_BQP_Display {
+    private $product_data;
+    public function __construct( VS_BQP_Product_Data $product_data ) {
+        $this->product_data = $product_data;
+    }
+}
